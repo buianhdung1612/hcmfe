7af95a5be9cd41126934ca4424c93c3a96cb3c42
Bui Anh Dung <buianh09dung@gmail.com>
2026-06-05 23:30:55 +0700

    Update backend URL to correct Vercel link

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,3 @@
 import { io } from 'socket.io-client';
 
-export const socket = io('https://hcm202-4k74noo13-bui-anh-dungs-projects.vercel.app');
+export const socket = io('https://hcm202-omega.vercel.app/');
